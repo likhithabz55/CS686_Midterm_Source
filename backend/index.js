3a9eb34f8e5ab6baab2ad65e30cf0d2f51903d54
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-02 05:12:15 -0800

    adjusting for cors

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ const db = mysql.createConnection({
 
 app.use(express.json())//return json data using the api server postman
 
+const cors = require('cors');//added
 app.use(cors())
 
 app.get("/", (req,res)=>{
