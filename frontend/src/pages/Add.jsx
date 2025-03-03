ddcc3a78e2ac1a6e4bcc193f6e05b9bb914fa428
Likhithabz <161535522+Likhithabz@users.noreply.github.com>
2025-03-02 23:24:53 -0800

    Updated paths for front end with proxy backend in .jsx files

--- frontend/src/pages/Add.jsx
+++ frontend/src/pages/Add.jsx
@@ -20,7 +20,7 @@ const Add = () => {
    const handleClick = async e =>{
     e.preventDefault()
     try{
-        await axios.post(`http://localhost:8800/books`, book)
+        await axios.post(`http://lamabooks.duckdns.org/backend/books`, book)
         navigate("/")
     }catch(err){
         console.log(err)
